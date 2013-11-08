992033de19d2103657e09d7cc2d3f3da1471c883
Sondre Naustdal <sondre.naustdal@gmail.com>
2013-11-08 23:56:30 +0100

    Scene now uses pure json unmarshall,
    Since this nagged me a little, I changed it to use purel a unmarshall
    method that was not nearly as good when I first started out this
    project.

--- src/rayTracer/scene.go
+++ src/rayTracer/scene.go
@@ -28,7 +28,7 @@ type Sphere struct {
 type Blob struct {
 	Centers       []Point
 	invSizeSquare float64
-	Size          int
+	Size          float64
 	Material      int
 }
 type Light struct {
@@ -42,14 +42,15 @@ type ImageS struct {
 	Height int
 }
 type Scene struct {
-	Name      string
-	Materials []Material
-	Spheres   []Sphere
-	Blobs     []Blob
-	Lights    []Light
-	Exposure  float64
-	CM        CubeMap
-	Image     ImageS
+	Name        string
+	CubeMapFile string
+	Materials   []Material
+	Spheres     []Sphere
+	Blobs       []Blob
+	Lights      []Light
+	Exposure    float64
+	CM          CubeMap
+	Image       ImageS
 }
 
 func contents(filename string) ([]byte, error) {
@@ -106,130 +107,35 @@ func NewScene(file string) (s *Scene, err error) {
 	if err != nil {
 		return
 	}
-	var f interface{}
-	err = json.Unmarshal(cfgJson, &f)
-
-	if err != nil {
-		return
-	}
-	println("Unmarshaled scene")
-	m := f.(map[string]interface{})
 	s = new(Scene)
-	sc := m["Scene"].(map[string]interface{})
-	img := sc["Image"].(map[string]interface{})
-	s.Name = sc["Name"].(string)
-	cmFn := sc["CubeMap"].(string)
-	cmF, err := os.Open("../img/" + cmFn)
-	if err != nil {
-		return
-	}
-	// format := ""
-	cmImg, format, err := image.Decode(cmF)
-	fmt.Println("CubeMap Format is ", format)
+	err = json.Unmarshal(cfgJson, &s)
 	if err != nil {
 		return
 	}
 
+	cmF, err := os.Open("../img/" + s.CubeMapFile)
+	if err != nil {
+		return
+	}
+	cmImg, _, err := image.Decode(cmF)
+	if err != nil {
+		return
+	}
 	s.CM = CubeMapInit(cmImg)
-	// fmt.Println("CubeMap Format is ",format)
-	s.Image = ImageS{int(img["Width"].(float64)), int(img["Height"].(float64))}
-
-	materials := sc["Materials"].([]interface{})
-	for i := range materials {
-		l := materials[i].(map[string]interface{})
-		d := l["Diffuse"].([]interface{})
-		d2 := l["Diffuse2"].([]interface{})
-		spec := l["Specular"].([]interface{})
-		r := l["Reflection"].(float64)
-		p := l["Power"].(float64)
-		bump := l["Bump"].(float64)
-		typ := l["Type"].(string)
-		df := make([]float64, 0)
-		for j := range d {
-			df = append(df, d[j].(float64))
-
-		}
-		df2 := make([]float64, 0)
-		for j := range d2 {
-			df2 = append(df2, d2[j].(float64))
-
-		}
-		specular := make([]float64, 0)
-		for j := range spec {
-			specular = append(specular, spec[j].(float64))
-
-		}
-
-		s.Materials = append(s.Materials, Material{df, df2, ColorV{df[0], df[1], df[2]}, r, specular, p, bump, typ})
-	}
-
-	lights := sc["Lights"].([]interface{})
-	for i := range lights {
-		l := lights[i].(map[string]interface{})
-		p := l["Position"].([]interface{})
-		in := l["Intensity"].([]interface{})
-		pf := make([]float64, 0)
-		for j := range p {
-			pf = append(pf, p[j].(float64))
-		}
-		inf := make([]float64, 0)
-		for j := range in {
-			inf = append(inf, in[j].(float64))
-		}
-		s.Lights = append(s.Lights, Light{pf, Point{pf[0], pf[1], pf[2]}, inf, ColorV{inf[0], inf[1], inf[2]}})
-	}
-
-	spheres := sc["Spheres"].([]interface{})
-	for i := range spheres {
-		l := spheres[i].(map[string]interface{})
-		c := l["Center"].([]interface{})
-
-		cf := make([]float64, 0)
-		for j := range c {
-			cf = append(cf, c[j].(float64))
-		}
-		si := int(l["Size"].(float64))
-		ma := int(l["Material"].(float64))
-		println("Sphere", si, ma)
-		s.Spheres = append(s.Spheres, Sphere{cf, Point{cf[0], cf[1], cf[2]}, si, ma})
-	}
-
+	s.Image = ImageS{int(s.Image.Width), int(s.Image.Height)}
 	InitBlobZones()
-	blobs := sc["Blobs"].([]interface{})
-	for i := range blobs {
-		l := blobs[i].(map[string]interface{})
-		cs := l["Centers"].([]interface{})
-
-		pts := make([]Point, 0)
-
-		si := int(l["Size"].(float64))
-		ma := int(l["Material"].(float64))
-		// println(cfs, cf, cs, si, ma)
-		for _, c := range cs {
-			floats := c.([]interface{})
-			cf := make([]float64, 0)
-
-			for j := range floats {
-				cf = append(cf, floats[j].(float64))
-
-			}
-			pts = append(pts, Point{cf[0], cf[1], cf[2]})
-		}
-
-		s.Blobs = append(s.Blobs, Blob{pts, 1.0 / float64(si*si), si, ma})
-	}
-
-	println(s.Blobs[0].Centers[0].X)
+	println("Unmarshaled scene")
 	// s.Image = ImageS{img["Width"].(float64) ,img["Height"].(float64)}
-	// println(s.Image.Width)
+	// fmt.Printf("%+v\n", s)
 
+	// println("Unmarshaled scene")
 	// jsonCheck(f)
 
 	return s, nil
 
 }
 
-func (s *Scene) String() string {
+func (s *Scene) Strings() string {
 	st := "Scene: \n"
 	st += "Image : \n"
 	st += "\tWidth " + fmt.Sprint(s.Image.Width) + "\n"
